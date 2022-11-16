6d62f7ca4cb369a0ce033f95157fa44352a8fb1d
Gabriel <gabrielsolanodesixto@gmail.com>
2022-11-16 11:16:50 -0600

    actulizacion 2.0

--- Launch-X-Latam JavaScript/pokedex.js
+++ Launch-X-Latam JavaScript/pokedex.js
@@ -1,29 +1,39 @@
-const fetchPokemon = () => {
+const fetchPokemon = async() => {
     const pokeNameInput = document.getElementById("pokeName");
     let pokeName = pokeNameInput.value;
     pokeName = pokeName.toLowerCase();
     const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
-    fetch(url).then((res) => {
+   //para consultar APIS
+   let data = await fetch(url).then((res) => {
         if (res.status != "200") {
             console.log(res);
-            pokeImage("./pokemon-sad.gif")
+            pokeImage("./pictures/pokemon-sad.jpeg")
         }
         else {
             return res.json();
         }
-    }).then((data) => {
+    })
         if (data) {
             console.log(data);
             let pokeImg = data.sprites.front_default;
+            let pokeInfo = data.abilities;
             pokeImage(pokeImg);
+            pokeData(pokeInfo);
             console.log(pokeImg);
-        }
-    });
+        
+    }
 }
 
 const pokeImage = (url) => {
     const pokePhoto = document.getElementById("pokeImg");
     pokePhoto.src = url;
+}
+
+const pokeData =(abilities)=>{
+    const pokeAbilites = document.getElementById("abilities");
+    const abilitiesName = abilities.map(item => item.ability.name);
+    //pokeAbilites.innerHTML = '<p>'+abilitiesName[0]+'</p>'; 
+    pokeAbilites.innerHTML = abilitiesName
 }
 
 
